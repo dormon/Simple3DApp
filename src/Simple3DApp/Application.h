4374da7f0c4abab7107a47a8e5125684f6be0035
Tomáš Milet <amillhouse@seznam.cz>
2018-10-17 23:23:49 +0200

    Added Fwd header

--- src/Simple3DApp/Application.h
+++ src/Simple3DApp/Application.h
@@ -1,29 +1,28 @@
 #pragma once
 
 #include <SDL2CPP/Window.h>
-#include <imguiSDL2OpenGL/imgui.h>
 #include <Simple3DApp/simple3dapp_export.h>
+#include <Simple3DApp/Fwd.h>
+#include <imguiSDL2OpenGL/imgui.h>
 
-namespace simple3DApp{
+class simple3DApp::Application {
+ public:
+  SIMPLE3DAPP_EXPORT Application(int argc, char* argv[]);
+  SIMPLE3DAPP_EXPORT virtual ~Application();
+  SIMPLE3DAPP_EXPORT void         start();
+  SIMPLE3DAPP_EXPORT void         swap();
+  SIMPLE3DAPP_EXPORT virtual void mouseMove(SDL_Event const& e);
+  SIMPLE3DAPP_EXPORT virtual void key(SDL_Event const& e, bool down);
+  SIMPLE3DAPP_EXPORT virtual void resize(uint32_t x, uint32_t y);
+  SIMPLE3DAPP_EXPORT virtual void draw();
+  SIMPLE3DAPP_EXPORT virtual void init();
+  SIMPLE3DAPP_EXPORT virtual void deinit();
 
-class SIMPLE3DAPP_EXPORT Application{
-  public:
-    Application(int argc,char*argv[]);
-    virtual ~Application();
-    void start();
-    void swap();
-    virtual void mouseMove(SDL_Event const&e);
-    virtual void key(SDL_Event const&e,bool down);
-    virtual void resize(uint32_t x,uint32_t y);
-    virtual void draw();
-    virtual void init();
-    virtual void deinit();
-  protected:
-    std::shared_ptr<sdl2cpp::MainLoop>mainLoop;
-    std::shared_ptr<sdl2cpp::Window>window;
-    std::unique_ptr<imguiSDL2OpenGL::Imgui>imgui;
-    int argc;
-    char**argv;
+ protected:
+  std::shared_ptr<sdl2cpp::MainLoop>      mainLoop;
+  std::shared_ptr<sdl2cpp::Window>        window;
+  std::unique_ptr<imguiSDL2OpenGL::Imgui> imgui;
+  int                                     argc;
+  char**                                  argv;
 };
 
-}
